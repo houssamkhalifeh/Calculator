4a1374d3db4e2bca94a1b865f8dcb2afffb184d0
Houssam <houssamkhalifeh@hotmail.com>
2022-10-10 15:56:34 -0400

    Change 3
    Signed-off-by: Houssam <houssamkhalifeh@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace Calculator
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bye World!");
             var a = 1;
+            
+        }
+        static bool foo(bool a, bool b, bool c, bool d, bool e, bool f, bool g)
+        {
+            bool x = a || b || c || d || e || f || g; // OK
+            bool y = a && b || !(b && c) || !(d && e) && !(f && g); // NOT OK
+            bool z = (a && b || (b && c)) && ((d && e) || (f && g)); // NOT OK
+            return x && y && z; // OK
         }
     }
 }
