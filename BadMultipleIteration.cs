5f08b5ee32fbe79db32df889baf534d2a82be356
Houssam <houssamkhalifeh@hotmail.com>
2022-10-12 12:02:41 -0400

    Change11

---
+++ BadMultipleIteration.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+class BadMultipleIteration
+{
+    private static int count = 1;
+
+    private static IEnumerable<int> NonRepeatable()
+    {
+        for (; count <= 3; count++)
+        {
+            yield return count;
+        }
+    }
+
+    public static void Test(string[] args)
+    {
+        IEnumerable<int> nr = NonRepeatable();
+        foreach (int i in nr)
+            Console.WriteLine(i);
+
+        foreach (int i in nr)
+            Console.WriteLine(i);
+    }
+}
+}
